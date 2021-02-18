415df9a2a9de7307665849dc7a82e09952e474b9
Amelia Schulz <amelia.schulz@gmail.com>
2021-02-18 17:21:29 -0500

    Adjust root card styles.

--- src/views/CarList.jsx
+++ src/views/CarList.jsx
@@ -1,17 +1,17 @@
-import { Card, CardContent, Grid } from "@material-ui/core";
+import { Card, CardContent, Grid, Typography } from "@material-ui/core";
 import React from "react";
 import { carData } from "../data/test-data";
 
 const CarList = () => {
   return (
     <div>
-      <Grid container spacing={3}>
+      <Grid container spacing={2}>
         {carData().map((car, index) => {
           return (
             <Grid item md={2} sm={3} xs={6}>
             <Card key={index}>
               <CardContent>
-                <p>{car["Heading"]}</p>
+                <Typography variant='body1'>{car["Heading"]}</Typography>
               </CardContent>
             </Card>
             </Grid>
