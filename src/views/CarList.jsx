4c0e192434dbfc8a347d33545d0a248b1f14fab4
Amelia Schulz <amelia.schulz@gmail.com>
2021-02-18 16:45:06 -0500

    Add material ui.

--- src/views/CarList.jsx
+++ src/views/CarList.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { Card, CardContent, Grid } from "@material-ui/core";
+import React from "react";
+import { carData } from "../data/test-data";
 
 const CarList = () => {
   return (
     <div>
-      cars go here
+      <Grid container spacing={3}>
+        {carData().map((car, index) => {
+          return (
+            <Grid item md={2} sm={3} xs={6}>
+            <Card key={index}>
+              <CardContent>
+                <p>{car["Heading"]}</p>
+              </CardContent>
+            </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default CarList
+export default CarList;
