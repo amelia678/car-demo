94bc8e214c4adff730ca7337507d2779a69628d7
Amelia Schulz <amelia.schulz@gmail.com>
2021-02-18 21:04:39 -0500

    Refactor card into it's own component.

---
+++ src/components/ListCard.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Divider,
+  Grid,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+
+const useStyles = makeStyles({
+  content: {
+    height: 220,
+  },
+  price: {
+    color: "green",
+    fontWeight: "bold",
+  },
+});
+
+const ListCard = (props) => {
+  const { car } = props;
+
+  const classes = useStyles();
+  return (
+    <Grid md={2} sm={3} xs={6} item>
+      <Card>
+        <CardMedia
+          component="img"
+          src={
+            "https://www.autotrader.com/content/dam/autotrader/homepage/styles/DefaultDesk_Style_LUXURY.png"
+          }
+          title="Car image"
+        />
+        <CardContent>
+          <Grid
+            container
+            direction="column"
+            justify="space-between"
+            className={classes.content}
+          >
+            <Grid item>
+              <Typography variant="h6">{car.Heading}</Typography>
+              <Box my={1}>
+                <Typography variant="subtitle2">{car.Subheading}</Typography>
+              </Box>
+            </Grid>
+            <Grid item>
+              <Divider></Divider>
+              <Box display="flex" justifyContent="flex-end">
+                <p className={classes.price}>
+                  ${car.Price.toLocaleString("en")}
+                </p>
+              </Box>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+};
+
+export default ListCard;
--- src/views/CarList.jsx
+++ src/views/CarList.jsx
@@ -1,65 +1,15 @@
-import {
-  Box,
-  Card,
-  CardContent,
-  CardMedia,
-  Divider,
-  Grid,
-  makeStyles,
-  Typography,
-} from "@material-ui/core";
 import React from "react";
+import { Grid } from "@material-ui/core";
+
+import ListCard from "../components/ListCard";
 import { carData } from "../data/test-data";
 
-const useStyles = makeStyles({
-  price: {
-    color: "green",
-    fontWeight: 'bold'
-  },
-});
-
 const CarList = () => {
-  const classes = useStyles();
   return (
     <div>
       <Grid container spacing={2}>
         {carData().map((car, index) => {
-          return (
-            <Grid key={index} md={2} sm={3} xs={6} item>
-              <Card>
-                <CardMedia
-                  component="img"
-                  src={
-                    "https://www.autotrader.com/content/dam/autotrader/homepage/styles/DefaultDesk_Style_LUXURY.png"
-                  }
-                  title="Car image"
-                />
-                <CardContent>
-                  <Grid
-                    container
-                    direction="column"
-                    justify="space-between"
-                    style={{ height: 220 }}
-                  >
-                    <Grid item>
-                      <Typography variant="h6">{car.Heading}</Typography>
-                      <Box my={1}>
-                        <Typography variant="subtitle2">
-                          {car.Subheading}
-                        </Typography>
-                      </Box>
-                    </Grid>
-                    <Grid item>
-                      <Divider></Divider>
-                      <Box display="flex" justifyContent="flex-end">
-                        <p className={classes.price}>${car.Price.toLocaleString("en")}</p>
-                      </Box>
-                    </Grid>
-                  </Grid>
-                </CardContent>
-              </Card>
-            </Grid>
-          );
+          return <ListCard car={car} key={index} />;
         })}
       </Grid>
     </div>
