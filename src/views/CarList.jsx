5cee62f4c7f26eefb6e5bd0a40ee59edc38bd6a5
Amelia Schulz <amelia.schulz@gmail.com>
2021-02-18 20:32:40 -0500

    Align items within card content.

--- src/views/CarList.jsx
+++ src/views/CarList.jsx
@@ -1,4 +1,12 @@
-import { Card, CardContent, Grid, Typography } from "@material-ui/core";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Divider,
+  Grid,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
 import { carData } from "../data/test-data";
 
@@ -8,12 +16,39 @@ const CarList = () => {
       <Grid container spacing={2}>
         {carData().map((car, index) => {
           return (
-            <Grid item md={2} sm={3} xs={6}>
-            <Card key={index}>
-              <CardContent>
-                <Typography variant='body1'>{car["Heading"]}</Typography>
-              </CardContent>
-            </Card>
+            <Grid key={index} md={2} sm={3} xs={6} item>
+              <Card>
+                <CardMedia
+                  component="img"
+                  src={
+                    "https://www.autotrader.com/content/dam/autotrader/homepage/styles/DefaultDesk_Style_LUXURY.png"
+                  }
+                  title="Car image"
+                />
+                <CardContent>
+                  <Grid
+                    container
+                    direction="column"
+                    justify="space-between"
+                    style={{ height: 220 }}
+                  >
+                    <Grid item>
+                      <Typography variant="h6">{car.Heading}</Typography>
+                      <Box my={1}>
+                        <Typography variant="subtitle2">
+                          {car.Subheading}
+                        </Typography>
+                      </Box>
+                    </Grid>
+                    <Grid item>
+                      <Divider></Divider>
+                      <Box display="flex" justifyContent="flex-end">
+                        <p>{car.Price}</p>
+                      </Box>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+              </Card>
             </Grid>
           );
         })}
