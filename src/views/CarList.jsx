5b7285e09a036ea9a5944e5b826290b23cb4eba2
Amelia Schulz <amelia.schulz@gmail.com>
2021-02-19 13:50:57 -0500

    Style sort select.

--- src/views/CarList.jsx
+++ src/views/CarList.jsx
@@ -1,8 +1,21 @@
 import React, { useState } from "react";
-import { Box, Grid, MenuItem, Select } from "@material-ui/core";
+import {
+  Box,
+  Grid,
+  InputLabel,
+  makeStyles,
+  MenuItem,
+  Select,
+} from "@material-ui/core";
 
 import ListCard from "../components/ListCard";
 import { carData } from "../data/test-data";
+
+const useStyles = makeStyles({
+  formControl: {
+    width: "100%",
+  },
+});
 
 const CarList = () => {
   const [cars, setCars] = useState(carData());
@@ -20,15 +33,29 @@ const CarList = () => {
     sortCars(e.target.value);
   };
 
+  const classes = useStyles();
+
   return (
     <div>
-      <Box py={2}>
-        <Select value={sort} onChange={handleChage}>
-          <MenuItem value="Heading">Heading</MenuItem>
-          <MenuItem value="Subheading">Subheading</MenuItem>
-          <MenuItem value="Price">Price</MenuItem>
-        </Select>
-      </Box>
+      <Grid container>
+        <Grid item md={4} sm={6} xs={12}>
+          <Box py={2}>
+            <InputLabel id="sort-select-label">Sort By</InputLabel>
+            <Select
+              ladbelId="sort-select-label"
+              id="sort-select"
+              value={sort}
+              onChange={handleChage}
+              className={classes.formControl}
+            >
+              <MenuItem value=''>None</MenuItem>
+              <MenuItem value="Heading">Heading</MenuItem>
+              <MenuItem value="Subheading">Subheading</MenuItem>
+              <MenuItem value="Price">Price</MenuItem>
+            </Select>
+          </Box>
+        </Grid>
+      </Grid>
       <Grid container spacing={2}>
         {cars.map((car, index) => {
           return <ListCard car={car} key={index} />;
