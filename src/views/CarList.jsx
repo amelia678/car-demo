9ac43755afa2657135a85bb4dfe0469f5863fb14
Amelia Schulz <amelia.schulz@gmail.com>
2021-02-19 13:28:30 -0500

    Add sort functionality.

--- src/views/CarList.jsx
+++ src/views/CarList.jsx
@@ -1,14 +1,36 @@
-import React from "react";
-import { Grid } from "@material-ui/core";
+import React, { useState } from "react";
+import { Box, Grid, MenuItem, Select } from "@material-ui/core";
 
 import ListCard from "../components/ListCard";
 import { carData } from "../data/test-data";
 
 const CarList = () => {
+  const [cars, setCars] = useState(carData());
+  const [sort, setSort] = useState("");
+
+  const sortCars = (filter) => {
+    const sortedCars = carData().sort((a, b) =>
+      a[filter] > b[filter] ? 1 : -1
+    );
+    setCars(sortedCars);
+  };
+
+  const handleChage = (e) => {
+    setSort(e.target.value);
+    sortCars(e.target.value);
+  };
+
   return (
     <div>
+      <Box py={2}>
+        <Select value={sort} onChange={handleChage}>
+          <MenuItem value="Heading">Heading</MenuItem>
+          <MenuItem value="Subheading">Subheading</MenuItem>
+          <MenuItem value="Price">Price</MenuItem>
+        </Select>
+      </Box>
       <Grid container spacing={2}>
-        {carData().map((car, index) => {
+        {cars.map((car, index) => {
           return <ListCard car={car} key={index} />;
         })}
       </Grid>
